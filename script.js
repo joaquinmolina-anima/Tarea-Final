d959d1f4fb976638d49173f747f03bfe604daf67
JoaquinMolina123 <joaquin.molina@anima.edu.uy>
2025-11-25 22:20:15 -0300

    sad
    tarea final

---
+++ script.js
@@ -0,0 +1,42 @@
+let loadMoreBtn1 = document.querySelector("#load-more-1");
+let currentItem1 = 4;
+
+loadMoreBtn1.onclick = () => {
+  let boxes = [...document.querySelectorAll(".box-container-1 .box-1")];
+
+  for (let i = currentItem1; i < currentItem1 + 4 && i < boxes.length; i++) {
+    boxes[i].style.display = "block";
+  }
+
+  currentItem1 += 4;
+
+  if (currentItem1 >= boxes.length) {
+    loadMoreBtn1.style.display = "none";
+  }
+};
+
+let loadMoreBtn2 = document.querySelector("#load-more-2");
+let currentItem2 = 4;
+loadMoreBtn2.onclick = () => {
+  let boxes = [...document.querySelectorAll(".box-container-2 .box-2")];
+  for (let i = currentItem2; i < currentItem2 + 4 && i < boxes.length; i++) {
+    boxes[i].style.display = "block";
+  }
+  currentItem2 += 4;
+  if (currentItem2 >= boxes.length) {
+    loadMoreBtn2.style.display = "none";
+  }
+};
+
+let loadMoreBtn3 = document.querySelector("#load-more-3");
+let currentItem3 = 4;
+loadMoreBtn3.onclick = () => {
+  let boxes = [...document.querySelectorAll(".box-container-3 .box-3")];
+  for (let i = currentItem3; i < currentItem3 + 4 && i < boxes.length; i++) {
+    boxes[i].style.display = "block";
+  }
+  currentItem3 += 4;
+  if (currentItem3 >= boxes.length) {
+    loadMoreBtn3.style.display = "none";
+  }
+};
